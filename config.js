6fcadcf40dfbc8217bd91f3525fab1ce843f599b
javik223 <javik@live.com>
2017-06-07 19:48:04 +0100

    Hello World

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'port': process.env.PORT || 8080,
+  'port': process.env.PORT || 3000,
   'database': 'mongodb://192.168.33.10:27017/mymongoapp',
   'secret': 'ilovescotchscotchyscotchscotch',
 };
